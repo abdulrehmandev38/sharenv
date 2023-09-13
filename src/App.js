0c7d5ab7893af40b626617d75e90b84cf09713d5
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-09-13 14:33:04 -0400

    add: routes & components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import { CreateVariables, Login, Permissions, Variables } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div className="w-full h-[90vh] flex items-center justify-center">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/variables" element={<Variables />} />
+          <Route path="/permissions" element={<Permissions />} />
+          <Route path="/new" element={<CreateVariables />} />
+          <Route path="/:variableID" element={<CreateVariables />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
