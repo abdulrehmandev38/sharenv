76321e6cda0cdaf9e212e9acf47f77a437e43a30
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-09-13 14:32:51 -0400

    :fire:

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as CreateVariables } from "./CreateVariables";
+export { default as Login } from "./Login";
+export { default as Variables } from "./Variables";
+export { default as Permissions } from "./Permissions";
