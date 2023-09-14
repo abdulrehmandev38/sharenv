0635bf416d30ddcfbc8c2f05fd10769a1403f96c
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-09-14 13:40:24 -0400

    delete: permissions comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as CreateVariables } from "./CreateVariables";
 export { default as Login } from "./Login";
 export { default as Variables } from "./Variables";
-export { default as Permissions } from "./Permissions";
