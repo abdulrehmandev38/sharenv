d0e731128e408c9da3c134ef505b45b561e36e10
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-09-14 13:40:10 -0400

    add: VariablesForm to CreateVariables page

--- src/pages/CreateVariables.jsx
+++ src/pages/CreateVariables.jsx
@@ -1,4 +1,10 @@
+import VariablesForm from "../components/VariablesForm";
+
 const CreateVariables = () => {
-  return <div></div>;
+  return (
+    <div className="border flex rounded-md items-center flex-col justify-center px-16 py-4">
+      <VariablesForm />
+    </div>
+  );
 };
 export default CreateVariables;
