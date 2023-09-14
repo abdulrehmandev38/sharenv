53508296b31d2d71963e6f5e8ee70a2b834f7875
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-09-14 13:39:35 -0400

    add: variables list

--- src/components/VariablesList.jsx
+++ src/components/VariablesList.jsx
@@ -1,4 +1,10 @@
+const VariablesItem = ({ name }) => {
+  return <div className="border px-4 py-2">{name}</div>;
+};
+
 const VariablesList = () => {
-  return <div></div>;
+  return Array(8)
+    .fill(0)
+    .map((e) => <VariablesItem />);
 };
 export default VariablesList;
